22ebac5e28f4f5fb41ff0fe73b8ddc52020d82a3
merttrkr <mertturker99@gmail.com>
2023-05-01 15:54:18 +0300

    Update institution condition form

--- components/form-components/InstitutionConditionsForm.tsx
+++ components/form-components/InstitutionConditionsForm.tsx
@@ -1,38 +1,37 @@
 import {
   Box,
   Button,
-  Container,
+  Checkbox,
   Flex,
   HStack,
   Heading,
-  Input,
-  Select,
-  SelectField,
   Stack,
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
-import { MdArrowDropDown } from 'react-icons/md';
-import SelectAutoComplete from '@/components/form-components/SelectAutoComplete';
 
+import SelectAutoComplete from '@/components/form-components/SelectAutoComplete';
+import TextInput from '@/components/form-components/TextInput';
+import CheckBoxInput from '@/components/form-components/CheckBoxInput';
 type InstitutionConditionsFormProps = {
   pageName: String;
-  subText:String;
+  subText: String;
 };
 
 export default function InstitutionConditionsForm({
-  pageName,subText,
+  pageName,
+  subText,
 }: InstitutionConditionsFormProps) {
   const HeaderBackground = useColorModeValue('gray.50', 'gray.800');
   const BorderColor = useColorModeValue('gray.200', 'gray.600');
   const HeadingColor = useColorModeValue('gray.600', 'gray.100');
   const ButtonColor = useColorModeValue('#20558B', '#0E3051');
   const apos = "'";
-  
+
   const ButtonHoverColor = useColorModeValue('#9C1F23', 'gray.600');
   return (
     <Stack
-      margin= '5'
+      margin='5'
       marginBottom='20'
       pl={3}
       py={4}
@@ -53,35 +52,45 @@ export default function InstitutionConditionsForm({
       </Heading>
       <Text>{subText}</Text>
 
-      <Box as={'form'} mt={10} boxShadow={'lg'} padding={5} >
-      <Flex p='5'><SelectAutoComplete selectLabel='Koşul Seçiniz' /></Flex>
-      
+      <Box as={'form'} mt={10} boxShadow={'lg'} padding={5}>
+        <Flex p='5'>
+          <SelectAutoComplete selectLabel='Koşul Seçiniz' />
+        </Flex>
+
         <Flex>
-        
           <Stack w='50%' spacing={4} p='5'>
-          <SelectAutoComplete selectLabel='Gönderen Kurum / Üniversite' />
-          <SelectAutoComplete selectLabel='Alıcı Kurum / Üniversite' />
-          <SelectAutoComplete selectLabel='Gönderen Kurumdaki İletişim Kurulabilecek Yetkililer' />
-          <SelectAutoComplete selectLabel='Hangi Akademik Yıllar Arasında Başlıyor ?' />
-          <SelectAutoComplete selectLabel='Yıl Bazında Hareketlilik (mobilite) Sayısı' />
-          <SelectAutoComplete selectLabel='ISCED Kodu ve Konu Alanları' />
+            <SelectAutoComplete selectLabel='Gönderen Kurum / Üniversite' />
+            <SelectAutoComplete selectLabel='Alıcı Kurum / Üniversite' />
+            <SelectAutoComplete selectLabel='Gönderen Kurumdaki İletişim Kurulabilecek Yetkililer' />
+            <SelectAutoComplete selectLabel='Hangi Akademik Yıllar Arasında Başlıyor ?' />
+            <SelectAutoComplete selectLabel='Yıl Bazında Hareketlilik (mobilite) Sayısı' />
+            <SelectAutoComplete selectLabel='ISCED Kodu ve Konu Alanları' />
+            <TextInput
+              placeHolder='0'
+              textInputLabel='Yıl Bazında Toplam Ay sayısı'
+            />
           </Stack>
           <Stack w='50%' spacing={4} p='5'>
-          <SelectAutoComplete selectLabel='Gönderen Kurumun İlgili Bölümü / Departmanı' />
-          <SelectAutoComplete selectLabel='Alıcı Kurumun İlgili Bölümü / Departmanı' />
-          <SelectAutoComplete selectLabel='Hangi Akademik Yıllar Arasında Bitiyor ?' />
-          <SelectAutoComplete selectLabel='Gönderen Kurumun İlgili Bölümü / Departmanı' />
-          
-          <HStack spacing={4}>
-          <SelectAutoComplete selectLabel='İstenilen Yabancı Dil' />
-          <Box w={'50%'}>
-            <SelectAutoComplete  selectLabel='Yabancı Dil Seviyesi' />
-            </Box>
-          
-          </HStack>
-            
+            <SelectAutoComplete selectLabel='Gönderen Kurumun İlgili Bölümü / Departmanı' />
+            <SelectAutoComplete selectLabel='Alıcı Kurumun İlgili Bölümü / Departmanı' />
+            <SelectAutoComplete selectLabel='Hangi Akademik Yıllar Arasında Bitiyor ?' />
+            <SelectAutoComplete selectLabel='Gönderen Kurumun İlgili Bölümü / Departmanı' />
+            <CheckBoxInput
+              placeHolder='Karma'
+              checkBoxInputLabel='Karma Eğitim Olacaksa Aşağıdaki Kutucuğu İşaretleyiniz'
+            />
+            <HStack spacing={4}>
+              <SelectAutoComplete selectLabel='İstenilen Yabancı Dil' />
+              <Box w={'50%'}>
+                <SelectAutoComplete selectLabel='Yabancı Dil Seviyesi' />
+              </Box>
+            </HStack>
           </Stack>
         </Flex>
+        <Flex p='5'>
+          <SelectAutoComplete selectLabel='Öğrenim Seviyesini Seçiniz' />
+        </Flex>
+
         <Flex gap={3} justifyContent={'right'} pr={4}>
           <Button
             fontFamily={'heading'}
@@ -94,21 +103,34 @@ export default function InstitutionConditionsForm({
               boxShadow: 'xl',
             }}
           >
-            Submit
+            Kaydet
           </Button>
           <Button
             border={'2px'}
-            
             fontFamily={'heading'}
             mt={8}
-            w={'15%'}
+            w={'auto'}
+            
+            color={ButtonColor}
+            _hover={{
+              color: ButtonHoverColor,
+              bg: 'white',
+              boxShadow: 'xl',
+            }}
+          >
+            Aynı Koşulları Partnerime De Ekle
+          </Button>
+          <Button
+            border={'2px'}
+            fontFamily={'heading'}
+            mt={8}
+            w={'10%'}
             bg='white'
             color={ButtonColor}
             _hover={{
-              color:ButtonHoverColor,
+              color: ButtonHoverColor,
               bg: 'white',
               boxShadow: 'xl',
-              
             }}
           >
             Clear
