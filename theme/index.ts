aa32f2f50acb6effb49733fab881d792c0ff51c6
merttrkr <mertturker99@gmail.com>
2023-05-01 19:10:20 +0300

    delete input theme

--- theme/index.ts
+++ theme/index.ts
@@ -1,11 +1,9 @@
 // src/theme/index.ts
 import { extendTheme } from '@chakra-ui/react'
-import { inputTheme } from '@/theme/components/input'
 import { buttonTheme } from '@/theme/components/button'
 
 export const theme = extendTheme({
   components: { 
-    Input: inputTheme,
     Button: buttonTheme
   },
 })
