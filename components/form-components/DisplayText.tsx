1896e4e076dccff240f3861df252c4506f284600
ilaydaozel <ilaydaoozel@gmail.com>
2023-05-02 21:42:07 +0300

    Add DisplayText component

---
+++ components/form-components/DisplayText.tsx
@@ -0,0 +1,28 @@
+import { Stack, Heading, useColorModeValue, Text } from "@chakra-ui/react";
+
+type DisplayTextProps = {
+  label: String;
+  content: string;
+};
+
+export default function DisplayText({ label, content }: DisplayTextProps) {
+  const HeadingColor = useColorModeValue("gray.600", "gray.100");
+
+  return (
+    <Stack>
+      <Heading
+        pl="1"
+        as="h3"
+        size="sm"
+        fontWeight={"bold"}
+        noOfLines={1}
+        color={HeadingColor}
+      >
+        {label}
+      </Heading>
+      <Text pl={2} fontWeight={"medium"} color={"#20558B"}>
+        {content}
+      </Text>
+    </Stack>
+  );
+}
