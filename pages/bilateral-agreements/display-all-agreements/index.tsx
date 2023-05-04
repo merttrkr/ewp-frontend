92900923c5f668fb2f545f3adb57bedaefb6fe13
ilaydaozel <ilaydaoozel@gmail.com>
2023-05-05 00:06:57 +0300

    Add button to DisplayAgreements

--- pages/bilateral-agreements/display-all-agreements/index.tsx
+++ pages/bilateral-agreements/display-all-agreements/index.tsx
@@ -1,5 +1,13 @@
 import PreviewIIA from "@/components/PreviewIIA";
+import { Flex, Button } from "@chakra-ui/react";
 
 export default function DisplayAgreements() {
-  return <PreviewIIA IIA="1" />;
+  return (
+    <>
+      <Flex justify={"flex-end"} px={6} py={4}>
+        <Button variant="condition">Yeni Anlaşma Oluştur</Button>
+      </Flex>
+      <PreviewIIA IIA="1" />
+    </>
+  );
 }
