6171a1bd8a5445b666f9dc93bf8330d538b2d666
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-02 00:16:58 +0300

    Create SelectInstitution

---
+++ components/form-components/selectboxes/SelectInstitution.tsx
@@ -0,0 +1,105 @@
+import {
+  Flex,
+  FormControl,
+  Icon,
+  InputGroup,
+  InputRightElement,
+  Heading,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import {
+  AutoComplete,
+  AutoCompleteInput,
+  AutoCompleteItem,
+  AutoCompleteList,
+} from '@choc-ui/chakra-autocomplete';
+import { FiChevronRight, FiChevronDown } from 'react-icons/fi';
+import { Contact } from '@/models/contactResponse';
+import useAgreement from '@/hooks/useAgreements';
+const { GetContactInfoByHeiID } = useAgreement();
+
+type SelectContactProps = {
+  selectLabel: String;
+  placeHolder: string;
+  isDisabled?: boolean;
+  id?: string;
+  register: any;
+};
+
+const Select: React.FC<SelectContactProps> = ({
+  isDisabled = false,
+  selectLabel,
+  id = 'default-select',
+  placeHolder,
+  register,
+}) => {
+  const [contactArray, setContactArray] = useState([] as Contact[]);
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      const data = await (
+        await GetContactInfoByHeiID(
+          'https://localhost:5001/spGetUniversityContactsByHeiId?heiId=iyte.edu.tr'
+        )
+      ).contacts; // Call the fetchData function
+      if (data) {
+        setContactArray(data); // Update the state with the fetched data
+      }
+    };
+    fetchInitialData();
+  }, []);
+
+  const HeadingColor = useColorModeValue('gray.600', 'gray.100');
+
+  return (
+    <Flex justify='left' align='center' w='full'>
+      <FormControl>
+        <Heading
+          pl='1'
+          pb='2'
+          as='h3'
+          size='sm'
+          fontWeight={'bold'}
+          noOfLines={1}
+          color={HeadingColor}
+        >
+          <label htmlFor={id}>{selectLabel}</label>
+        </Heading>
+        <AutoComplete openOnFocus>
+          {({ isOpen }) => (
+            <>
+              <InputGroup>
+                <AutoCompleteInput
+                  id={id}
+                  {...register}
+                  disabled={isDisabled}
+                  variant='filled'
+                  placeholder={placeHolder}
+                />
+                <InputRightElement>
+                  {' '}
+                  <Icon as={isOpen ? FiChevronRight : FiChevronDown} />
+                </InputRightElement>
+              </InputGroup>
+              <AutoCompleteList>
+                {contactArray.map((element, cid) => (
+                  <AutoCompleteItem
+                    key={`option-${cid}`}
+                    value={element.fullName} // Render the fullName property
+                    textTransform='capitalize'
+                  >
+                    {element.fullName}
+                  </AutoCompleteItem>
+                ))}
+              </AutoCompleteList>
+            </>
+          )}
+        </AutoComplete>
+      </FormControl>
+    </Flex>
+  );
+};
+
+export default Select;
--- components/forms/InstitutionInformationFormIIA.tsx
+++ components/forms/InstitutionInformationFormIIA.tsx
@@ -3,7 +3,6 @@ import {
   Button,
   Flex,
   Heading,
-  Input,
   Stack,
   Text,
   useColorModeValue,
@@ -13,6 +12,8 @@ import TextInput from '../form-components/TextInput';
 import DatePickerInput from '../form-components/DatePickerInput';
 import { useForm } from 'react-hook-form';
 import React from 'react';
+//selectboxes
+import SelectContact from '../form-components/selectboxes/SelectContact';
 
 type InstitutionInformationFormProps = {
   pageName: string;
@@ -21,21 +22,20 @@ type InstitutionInformationFormProps = {
 
 type FormData = {
   instution_name: string;
-  contact_persons:string;
-  departmant_name:string;
+  contact_persons: string;
+  departmant_name: string;
 };
 
 export default function InstitutionInformationForm({
   pageName,
   subText,
 }: InstitutionInformationFormProps) {
+  //colors
   const HeaderBackground = useColorModeValue('gray.100', 'gray.800');
   const FormBackground = useColorModeValue('gray.50', 'gray.700');
   const BorderColor = useColorModeValue('gray.200', 'gray.600');
   const HeadingColor = useColorModeValue('gray.600', 'gray.100');
 
-  const initialRef = React.useRef(null);
-  const finalRef = React.useRef(null);
   const {
     handleSubmit,
     register,
@@ -102,7 +102,7 @@ export default function InstitutionInformationForm({
               name='Mert Türker'
               label='Anlaşmayı İmzalayacak Yetkili'
             />
-            <SelectAutoComplete
+            <SelectContact
               id='contact_persons'
               register={register('contact_persons', {
                 required: 'This is required',
@@ -116,15 +116,15 @@ export default function InstitutionInformationForm({
             />
           </Stack>
           <Stack w='50%' spacing={4} p='5'>
-          <SelectAutoComplete
-            id='departmant_name'
-            register={register('departmant_name', {
-              required: 'This is required',
-              minLength: {
-                value: 4,
-                message: 'Minimum length should be 4',
-              },
-            })}
+            <SelectAutoComplete
+              id='departmant_name'
+              register={register('departmant_name', {
+                required: 'This is required',
+                minLength: {
+                  value: 4,
+                  message: 'Minimum length should be 4',
+                },
+              })}
               placeHolder='placeholder..'
               selectLabel='Departman / Bölüm Adı'
             />
