115c0a81684f85c201e34e87617a45d3c632535d
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-01 17:39:09 +0300

    Create commitment and proposedMobilityProgramme models

---
+++ models/commitment.ts
@@ -0,0 +1,14 @@
+export interface Commitment {
+    sendingHeiId: string;
+    commitmentId: number;
+    studentSignature: string;
+    sendingHeiSignature: string;
+    sendingHeiResponsibleFullname: string;
+    sendingHeiResponsiblePosition: string;
+    sendingHeiResponsibleEmail: string;
+    receivingHeiSignature: string;
+    receivingHeiResponsibleFullname: string;
+    receivingHeiResponsiblePosition: string;
+    receivingHeiResponsibleEmail: string;
+    commentForRejection: string;
+};
---
+++ models/proposedMobilityProgramme.ts
@@ -0,0 +1,9 @@
+export interface ProposedMobilityProgramme {
+    pmpId: number;
+    plannedStartingDateOfMobility: Date;
+    plannedEndDateOfMobility: Date;
+    receivingInstitutionCourseCatalogueLink: string;
+    languageId: number;
+    languageLevelId: number;
+    provisionsLinkIfEducationUnsuccessful: string;
+}
