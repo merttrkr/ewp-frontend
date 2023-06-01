06847cf71a1e534d14911b46511f32efa555f03b
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-01 18:45:15 +0300

    create selectedCourse and virtualCourse models

---
+++ models/selectedCourse.ts
@@ -0,0 +1,13 @@
+export interface SelectedCourse {
+    courseTitle: string;
+    courseCreditType_id: number;
+    courseCreditValue: number;
+    numberOfTerms: number;
+    totalNumberOfTerms: number;
+    courseCode: string;
+    recognitionConditions: string;
+    courseShortDescription: string;
+    tableType: string;
+    isApproved: number;
+    proposedMobilityProgramme_id: number;
+}
---
+++ models/virtualCourse.ts
@@ -0,0 +1,13 @@
+export interface VirtualCourse {
+    courseTitle: string;
+    courseCreditType_id: number;
+    courseCreditValue: number;
+    numberOfTerms: number;
+    totalNumberOfTerms: number;
+    courseCode: string;
+    recognitionConditions: string;
+    courseShortDescription: string;
+    tableType: string;
+    isApproved: number;
+    virtualComponent_id: number;
+}
