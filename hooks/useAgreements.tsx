fbd3328a078441d7b0ae29399ac825435e32987f
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-01 22:44:02 +0300

    update useAgreement

--- hooks/useAgreements.tsx
+++ hooks/useAgreements.tsx
@@ -16,13 +16,34 @@ const useAgreement = () => {
       throw new Error(`Error! status: ${response.status}`);
     }
     const fetchedContacts: Contact[] = await response.json();
-    const contactList: ContactResponse = { Contacts: fetchedContacts };
+    const contactList: ContactResponse = { contacts: fetchedContacts };
 
     console.log('result: ', contactList);
     return contactList;
   };
 
-  // Add any additional functions or state variables related to the hook here
+  //https://localhost:5001/spGetUniversityNamesForOrganization?uniShortName=all
+  const GetAllUniversitiesInfo = async (
+    request: string
+  ): Promise<ContactResponse> => {
+    let response = await fetch(request, {
+      method: 'POST',
+      headers: {
+        Accept: 'text/plain',
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+    const fetchedContacts: Contact[] = await response.json();
+    const contactList: ContactResponse = { contacts: fetchedContacts };
+
+    console.log('result: ', contactList);
+    return contactList;
+  };
+
+  
 
   return {
     GetContactInfoByHeiID,
