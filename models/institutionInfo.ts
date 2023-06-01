3d2a8db232a9afd04a919e9080fae1d8d0946a30
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-01 18:13:12 +0300

    Create institutionInfo and studentInfo models

---
+++ models/institutionInfo.ts
@@ -0,0 +1,27 @@
+export interface ReceivingInstitutionInfo {
+    receivingInstitutionInfo_id: number;
+    university_id: number;
+    universityDepartment_id: number;
+    academicYear_id: number;
+    academicPersonnelName: string;
+    academicPersonnelSurname: string;
+    academicPersonnelEmail: string;
+    phoneNumberE164: string;
+    phoneNumberExt: string;
+}
+
+export interface SendingInstitutionInfo {
+    sendingInstitutionInfo_id: number;
+    hei_id: string;
+    universityDepartment_id: number;
+    academicPersonnelName: string;
+    academicPersonnelSurname: string;
+    academicPersonnelEmail: string;
+    administrativePersonnelName: string;
+    administrativePersonnelSurname: string;
+    administrativePersonnelEmail: string;
+    phoneNumberE164: string;
+    phoneNumberExt: string;
+}
+
+
---
+++ models/studentInfo.ts
@@ -0,0 +1,15 @@
+export interface StudentInfo {
+    studentInfo_id: number;
+    mobilityType_id: number;
+    name: string;
+    surname: string;
+    gender_id: number;
+    nationality_id: number;
+    birthdate: string;
+    educationTypeAndLevel_id: number;
+    email: string;
+    subjectArea_id: number;
+    subjectAreaDescription: string;
+    global_id: string;
+    omobility_id: string;
+}
