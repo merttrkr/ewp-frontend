5197c92751ea22738f28e7d417af51b99ae939d3
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-07 16:36:57 +0300

    Create organizationInfoResponse.ts

---
+++ models/response/organizationInfoResponse.ts
@@ -0,0 +1,11 @@
+export interface OrganizationInfo {
+    heiId: string;
+    uniName: string;
+    UniDepartmentId: number;
+    ounitName: string;
+    signingPersonFullName: string;
+    signingDate: string;
+    IIACode: string;
+    IIAID: string;
+  }
+  
