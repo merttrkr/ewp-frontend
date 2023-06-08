b6c8bca7cc36dcde998320c5ebcd23249ca795f5
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-08 22:47:02 +0300

    Update index.tsx

--- pages/bilateral-agreements/display-all-agreements/index.tsx
+++ pages/bilateral-agreements/display-all-agreements/index.tsx
@@ -1,13 +1,21 @@
+import React from 'react';
 import PreviewIIA from '@/components/PreviewIIA';
 import { Flex, Button } from '@chakra-ui/react';
 
 export default function DisplayAgreements() {
+  // Create an array of IIA values
+  const IIAs = ['1', '2', '3', '5', '6', '7', '8', '9', '10'];
+
   return (
     <>
       <Flex justify={'flex-end'} px={6} py={4}>
         <Button variant='condition'>Yeni Anlaşma Oluştur</Button>
       </Flex>
-      <PreviewIIA IIA='1' />
+
+      {/* Loop through the IIAs array and render PreviewIIA component for each IIA */}
+      {IIAs.map((IIA) => (
+        <PreviewIIA key={IIA} IIA={IIA} />
+      ))}
     </>
   );
 }
