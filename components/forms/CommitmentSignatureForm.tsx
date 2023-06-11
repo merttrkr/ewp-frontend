686705992d50638f52a5527a0602a5c70223074a
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 19:38:18 +0300

    Add form structure to CommitmentSignatureForm

--- components/forms/CommitmentSignatureForm.tsx
+++ components/forms/CommitmentSignatureForm.tsx
@@ -7,13 +7,24 @@ import {
   Stack,
   Text,
   useColorModeValue,
+  useToast,
 } from '@chakra-ui/react';
 
 import TextInput from '@/components/form-components/inputs/TextInput1';
 import SignatureInput from '../form-components/inputs/SignatureInput';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 type CommitmentSignatureFormProps = {
   pageName: String;
+};
+
+type FormData = {
+  signature: string;
+  personalName: string;
+  personalSurname: string;
+  personalEposta: string;
+  comment: string;
 };
 
 export default function CommitmentSignatureForm({
@@ -23,6 +34,44 @@ export default function CommitmentSignatureForm({
   const BorderColor = useColorModeValue('gray.200', 'gray.600');
   const HeadingColor = useColorModeValue('gray.600', 'gray.100');
   const FormBackground = useColorModeValue('gray.50', 'gray.700');
+  const toast = useToast();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    setValue,
+    control,
+  } = useForm<FormData>();
+
+  const onSubmit = (values: FormData) => {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        alert(JSON.stringify(values, null));
+        console.log('values: ', values);
+
+        toast({
+          title: 'Kayıt Başarılı.',
+          description: 'Form başarıyla kaydedildi.',
+          status: 'success',
+          position: 'top-right',
+          duration: 5000,
+          isClosable: true,
+        });
+        resolve();
+      } catch (error) {
+        toast({
+          title: 'Kayıt Başarısız.',
+          description: `${error}`,
+          status: 'error',
+          position: 'top-right',
+          duration: 5000,
+          isClosable: true,
+        });
+        console.log(error);
+        reject(error);
+      }
+    });
+  };
 
   return (
     <Stack
@@ -49,6 +98,7 @@ export default function CommitmentSignatureForm({
         padding={5}
         bg={FormBackground}
         borderRadius={'xl'}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <Flex>
           <Stack w='50%' spacing={4} p='5' borderRight={'1px solid lightgray'}>
@@ -117,8 +167,12 @@ export default function CommitmentSignatureForm({
           </Stack>
         </Flex>
         <Flex gap={3} justifyContent={'right'} pr={4} mt={'8'}>
-          <Button variant='submit'>Kaydet</Button>
-          <Button variant='clear'>Sıfırla</Button>
+          <Button variant='submit' type='submit'>
+            Kaydet
+          </Button>
+          <Button variant='clear' type='reset'>
+            Sıfırla
+          </Button>
         </Flex>
         <Flex pt={4} justifyContent={'right'}>
           <Button variant='autoWidthFull'>
