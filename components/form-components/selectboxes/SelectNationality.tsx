4fc87445cfbf4f78b2305dc59d9e719ab5f37371
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-11 20:19:11 +0300

    Create SelectNationality.tsx

---
+++ components/form-components/selectboxes/SelectNationality.tsx
@@ -0,0 +1,87 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { FormControl, useColorModeValue, Heading } from '@chakra-ui/react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import useRead from '@/hooks/read/useRead';
+import { Nationality } from '@/models/response/nationalityResponse';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+type SelectNationalityProps = {
+  selectLabel: string;
+  placeholder: string;
+  isDisabled?: boolean;
+  id?: string;
+  register: any;
+  onChange: (value: Nationality | null) => void; // New prop for handling value change
+  error: string | undefined;
+};
+
+const SelectNationality: React.FC<SelectNationalityProps> = ({
+  isDisabled = false,
+  selectLabel,
+  id = 'default-select',
+  placeholder,
+  register,
+  error,
+  onChange, // Add the new onChange prop
+}) => {
+  const { GetNationalities } = useRead();
+  const [nationalityArray, setNationalityArray] = useState<Nationality[]>([]);
+  const theme = createTheme({
+    // your theme configuration
+  });
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      try {
+        const result = await GetNationalities();
+        console.log('Result:', result);
+        if (result) {
+          setNationalityArray(result); // Update the state with the fetched data
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+    fetchInitialData();
+  }, []);
+
+  const HeadingColor = useColorModeValue('gray.600', 'gray.100');
+  return (
+    <ThemeProvider theme={theme}>
+      {
+        <FormControl>
+          <Heading
+            pl='1'
+            pb='2'
+            size='sm'
+            fontWeight={'bold'}
+            color={HeadingColor}
+          >
+            <label htmlFor={id}>{selectLabel}</label>
+          </Heading>
+          <Autocomplete
+            onChange={(event, value) => onChange(value || null)}
+            disablePortal
+            id={id}
+            options={nationalityArray}
+            getOptionLabel={(option) => option.nationality}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder={placeholder}
+                disabled={isDisabled}
+                inputRef={register}
+              />
+            )}
+          />
+          {error && <span style={{ color: 'red' }}>{error}</span>}
+        </FormControl>
+      }
+    </ThemeProvider>
+  );
+};
+
+export default SelectNationality;
