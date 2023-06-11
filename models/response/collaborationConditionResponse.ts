8afd665e391726deecd8bc6d27cb31416d63eb3f
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-11 16:12:26 +0300

    Create collaborationConditionResponse.ts

---
+++ models/response/collaborationConditionResponse.ts
@@ -0,0 +1,9 @@
+export interface CollaborationConditionResponse {
+    academicYear: string;
+    annualQuota: string;
+    otherInfo: string;
+    annualTotalMonths: string;
+    isCoEducational: string;
+    educationTypeAndLevel: string;
+    subjectArea: string;
+}
