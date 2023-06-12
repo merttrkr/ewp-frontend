ef6f0d77090a976a1fd3367d822d356e3673c78d
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-12 16:23:16 +0300

    Create courseResponse.ts

---
+++ models/response/courseResponse.ts
@@ -0,0 +1,12 @@
+export interface Course {
+    id: number;
+    courseCreditType: string;
+    courseTitle: string;
+    courseCreditValue: number;
+    numberOfTerms: number;
+    totalNumberOfTerms: number;
+    courseCode: string;
+    status: string;
+    recognitionConditions?: string;
+    courseShortDescription?: string;
+}
