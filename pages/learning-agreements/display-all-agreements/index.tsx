9eb7064c3b179ea427a75c0f018606b5c768a57f
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-12 19:20:24 +0300

    Update index.tsx

--- pages/learning-agreements/display-all-agreements/index.tsx
+++ pages/learning-agreements/display-all-agreements/index.tsx
@@ -1,12 +1,96 @@
 import React, { useEffect, useState } from 'react';
-import PreviewIIA from '@/components/PreviewIIA';
 import { Flex, Button, Input, Stack, Center } from '@chakra-ui/react';
-import TextInput from '@/components/form-components/inputs/TextInput';
 import { SearchIcon } from '@chakra-ui/icons';
 import useRead from '@/hooks/read/useRead';
-import { BilateralAgreement } from '@/models/response/bilateralAgreementResponse';
-import NextLink from 'next/link';
+import PreviewOLA from '@/components/PreviewOLA';
 
 export default function DisplayAgreements() {
-  return <></>;
+  const [currentPage, setCurrentPage] = useState(1); // Current page number
+  const [searchQuery, setSearchQuery] = useState(''); // Search query
+  const { GetAllLearningAgreements } = useRead();
+  const [learningAgreements, setLearningAgreements] = useState<
+    LearningAgreement[]
+  >([]);
+  const itemsPerPage = 3;
+  async function handleGetLearningAgreements() {
+    try {
+      const data = await GetAllLearningAgreements(
+        `https://localhost:5001/spGetAllLearningAgreements`
+      );
+      if (data != null) {
+        setLearningAgreements(data);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    handleGetLearningAgreements();
+  }, []);
+
+  const filteredAgreements = learningAgreements.filter((agreement) => {
+    const selectedMobilityType = agreement.mobilityType || '-';
+    const stıdentName = agreement.studentFullName || '-';
+    const lowerCaseQuery = searchQuery.toLowerCase();
+    return (
+      selectedMobilityType.toLowerCase().includes(lowerCaseQuery) ||
+      stıdentName.toLowerCase().includes(lowerCaseQuery)
+    );
+  });
+
+  const totalPages = Math.ceil(filteredAgreements.length / itemsPerPage);
+
+  const currentAgreements = filteredAgreements.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+    setCurrentPage(1); // Reset to the first page when the search query changes
+  };
+
+  return (
+    <>
+      {/* Search input field */}
+      <Flex align={'center'} justify={'flex-end'} px={6} py={4}>
+        <Stack justify={'right'} px={2} direction={'row'}>
+          <SearchIcon mt={3} color='gray.600' />
+          <Input
+            width='auto'
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </Stack>
+      </Flex>
+
+      {/* Loop through the currentAgreements array and render PreviewIIA component for each agreement */}
+      {currentAgreements.map((agreement) => (
+        <PreviewOLA
+          key={agreement.proposedMobilityProgramme_id}
+          agreement={agreement}
+        />
+      ))}
+
+      {/* Pagination controls */}
+      <Flex justify={'center'}>
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (pageNumber) => (
+            <Button
+              key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
+              variant={currentPage === pageNumber ? 'solid' : 'outline'}
+            >
+              {pageNumber}
+            </Button>
+          )
+        )}
+      </Flex>
+    </>
+  );
 }
