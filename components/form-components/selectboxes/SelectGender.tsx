a94333e925570d9a0fa2ae8fc1fda3a481024167
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-12 03:29:18 +0300

    Create SelectGender.tsx

---
+++ components/form-components/selectboxes/SelectGender.tsx
@@ -0,0 +1,84 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { FormControl, useColorModeValue, Heading } from '@chakra-ui/react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import useRead from '@/hooks/read/useRead';
+import { Gender } from '@/models/response/genderResponse';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+type SelectGenderProps = {
+  selectLabel: string;
+  placeholder: string;
+  isDisabled?: boolean;
+  id?: string;
+  register: any;
+  onChange: (value: Gender | null) => void; // New prop for handling value change
+  error: string | undefined;
+};
+
+const SelectGender: React.FC<SelectGenderProps> = ({
+  isDisabled = false,
+  selectLabel,
+  id = 'default-select',
+  placeholder,
+  register,
+  error,
+  onChange, // Add the new onChange prop
+}) => {
+  const { GetGenderNames } = useRead();
+  const [genderArray, setGenderArray] = useState<Gender[]>([]);
+  const theme = createTheme({
+    // your theme configuration
+  });
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      const data = await GetGenderNames(
+        'https://localhost:5001/spGetGenderNames'
+      ); // Call the GetGenderNames function
+      if (data) {
+        setGenderArray(data); // Update the state with the fetched data
+      }
+    };
+    fetchInitialData();
+  }, []);
+
+  const HeadingColor = useColorModeValue('gray.600', 'gray.100');
+  return (
+    <ThemeProvider theme={theme}>
+      {
+        <FormControl>
+          <Heading
+            pl='1'
+            pb='2'
+            size='sm'
+            fontWeight={'bold'}
+            color={HeadingColor}
+          >
+            <label htmlFor={id}>{selectLabel}</label>
+          </Heading>
+          <Autocomplete
+            onChange={(event, value) => onChange(value || null)}
+            disablePortal
+            id={id}
+            options={genderArray}
+            getOptionLabel={(option) => option.genderName}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                placeholder={placeholder}
+                disabled={isDisabled}
+                inputRef={register}
+              />
+            )}
+          />
+          {error && <span style={{ color: 'red' }}>{error}</span>}
+        </FormControl>
+      }
+    </ThemeProvider>
+  );
+};
+
+export default SelectGender;
