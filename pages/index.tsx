b9a5fe823b375919c5b4bce0ae8f78a2574be7e9
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-15 16:44:54 +0300

    Update index.tsx
    Add animation re design and responsivity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,57 @@
 import Image from 'next/image';
-import {
-  Box,
-  Flex,
-  Heading,
-  Text,
-  useColorMode,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Box, Flex, Heading, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 const logoSrc = '/logo_ewp.png';
 const darkLogoSrc = '/logo_ewp_dark.png';
 
 export default function Home() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const ContainerBackground = useColorModeValue('gray.100', 'gray.700');
+  const { colorMode } = useColorMode();
+  const containerBackground = useColorModeValue('gray.100', 'gray.700');
+  const accentColor = useColorModeValue('gray.700', 'gray.300');
+  const logoSize = colorMode === 'light' ? 200 : 250;
+  const logoAltText = 'EWP Logo';
+
   return (
     <Flex
-      width={'100%'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      paddingY={25}
+      width='100%'
+      height='100vh'
+      alignItems='center'
+      justifyContent='center'
+      bg={useColorModeValue('gray.200', 'gray.800')}
     >
       <Flex
-        justifyContent={'center'}
-        alignItems={'center'}
-        padding={'10%'}
-        borderBottom='1px'
-        bg={ContainerBackground}
-        borderColor={ContainerBackground}
-        borderRadius={'xl'}
-        width={'80%'}
-        gap={5}
-        flexDirection={'column'}
+        justifyContent='center'
+        alignItems='center'
+        padding='8%'
+        bg={containerBackground}
+        borderRadius='xl'
+        width={['90%', '80%']}
+        flexDirection='column'
+        boxShadow='2xl'
       >
-        <Image
-          width={150}
-          height={150}
-          src={colorMode === 'light' ? logoSrc : darkLogoSrc}
-          alt={'EWP Logo'}
-        />
-        <Box>
-          <Heading color='#9C1F23'>EWP IYTE&apos;ye hoş geldiniz!</Heading>
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          <Image
+            width={logoSize}
+            height={logoSize}
+            src={colorMode === 'light' ? logoSrc : darkLogoSrc}
+            alt={logoAltText}
+          />
+        </motion.div>
+        <Box mt={6}>
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: 0.3 }}
+          >
+            <Heading color={accentColor} fontSize={['4xl', '5xl']} textAlign='center'>
+              EWP IYTE'ye hoş geldiniz!
+            </Heading>
+          </motion.div>
         </Box>
       </Flex>
     </Flex>
