d9bf5e0a8bbce2237a3fe89c27d1265835b55a05
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-19 23:05:56 +0300

    Update index.tsx

--- pages/learning-agreements/create-blended-la/index.tsx
+++ pages/learning-agreements/create-blended-la/index.tsx
@@ -16,6 +16,10 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import useRead from '@/hooks/read/useRead';
+import { StudentInfoResponse } from '@/models/response/studentInfoResponse';
+
 
 
 export default function TabComponent() {
@@ -28,6 +32,12 @@ export default function TabComponent() {
     GenerateNewIdForProposedMobilityProgramme,
     GenerateNewIdForCommitment,
   } = useCreate();
+  const {
+    GetStudentInfoById,
+  } = useRead();
+  const router = useRouter();
+
+
 
   const [omobilityID, setOmobilityID] = useState('');
   const [learningAgreementID, setLearningAgreementID] = useState(0);
@@ -39,102 +49,185 @@ export default function TabComponent() {
     useState(0);
   const [commitmentID, setCommitmentID] = useState(0);
   const [mobilityTypeId, setMobilityTypeId] = useState(2);
+  const [studentInfo, setStudentInfo] = useState<StudentInfoResponse>();
 
   const HeadingColor = useColorModeValue('gray.600', 'gray.300');
 
+  const { studentInfoId , proposedMobilityProgrammeId, commitmentId } =
+  router.query;
+
+
+
+
+  
+
+  const handleGetStudentInfoById = async () => {
+    const request = 'https://localhost:5001/spGetStudentInfoById?studentInfo_id='+ studentInfoID ;
+    try {
+      const studentInfo = await GetStudentInfoById(request);
+      
+      console.log('Student Info:', studentInfo);
+      // Handle the received studentInfo data: update state, display to the user, etc.
+    } catch (error) {
+      console.error('Error fetching student info:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
+  };
+  
+  
   async function handleGenerateOmobilityId() {
-    const fetchOmobilityID = async () => {
-      const data = await GenerateOmobilityId(
-        'https://localhost:5001/spGenerateOmobilityId'
-      );
-      if (data) {
+    try {
+      const data = await GenerateOmobilityId('https://localhost:5001/spGenerateOmobilityId');
+      if (data !== null && data !== undefined) {
         setOmobilityID(data);
+      } else {
+        throw new Error('No data received for omobility ID');
       }
-    };
-    fetchOmobilityID();
+    } catch (error) {
+      console.error('Error generating omobility ID:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
   }
 
   async function handleGenerateNewIdForLearningAgreement() {
-    const fetchLearningAgreementID = async () => {
-      const data = await GenerateNewIdForLearningAgreement(
-        'https://localhost:5001/spGenerateNewIdForLearningAgreement'
-      );
-      if (data) {
+    try {
+      const data = await GenerateNewIdForLearningAgreement('https://localhost:5001/spGenerateNewIdForLearningAgreement');
+      if (data !== null && data !== undefined) {
         setLearningAgreementID(data);
+      } else {
+        throw new Error('No data received for learning agreement ID');
       }
-    };
-    fetchLearningAgreementID();
+    } catch (error) {
+      console.error('Error generating learning agreement ID:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
   }
 
   async function handleGenerateNewIdForStudentInfo() {
-    const fetchStudentInfoID = async () => {
-      const data = await GenerateNewIdForStudentInfo(
-        'https://localhost:5001/spGenerateNewIdForStudentInfo'
-      );
-      if (data) {
-        setStudentInfoID(data);
+    try {
+      const data = await GenerateNewIdForStudentInfo('https://localhost:5001/spGenerateNewIdForStudentInfo');
+      if (data !== null && data !== undefined) {
+        
+          setStudentInfoID(data);
+        
+        
+      } else {
+        throw new Error('No data received for student info ID');
       }
-    };
-    fetchStudentInfoID();
+    } catch (error) {
+      console.error('Error generating student info ID:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
   }
 
   async function handleGenerateNewIdForSendingInstitutionInfo() {
-    const fetchSendingInstitutionInfoID = async () => {
-      const data = await GenerateNewIdForSendingInstitutionInfo(
-        'https://localhost:5001/spGenerateNewIdForSendingInstitutionInfo'
-      );
-      if (data) {
+    try {
+      const data = await GenerateNewIdForSendingInstitutionInfo('https://localhost:5001/spGenerateNewIdForSendingInstitutionInfo');
+      if (data !== null && data !== undefined) {
         setSendingInstitutionInfoID(data);
+      } else {
+        throw new Error('No data received for sending institution info ID');
       }
-    };
-    fetchSendingInstitutionInfoID();
+    } catch (error) {
+      console.error('Error generating sending institution info ID:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
   }
 
   async function handleGenerateNewIdForReceivingInstitutionInfo() {
-    const fetchReceivingInstitutionInfoID = async () => {
-      const data = await GenerateNewIdForReceivingInstitutionInfo(
-        'https://localhost:5001/spGenerateNewIdForReceivingInstitutionInfo'
-      );
-      if (data) {
+    try {
+      const data = await GenerateNewIdForReceivingInstitutionInfo('https://localhost:5001/spGenerateNewIdForReceivingInstitutionInfo');
+      if (data !== null && data !== undefined) {
         setReceivingInstitutionInfoID(data);
+      } else {
+        throw new Error('No data received for receiving institution info ID');
       }
-    };
-    fetchReceivingInstitutionInfoID();
+    } catch (error) {
+      console.error('Error generating receiving institution info ID:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
   }
 
   async function handleGenerateNewIdForProposedMobilityProgramme() {
-    const fetchProposedMobilityProgrammeID = async () => {
-      const data = await GenerateNewIdForProposedMobilityProgramme(
-        'https://localhost:5001/spGenerateNewIdForProposedMobilityProgramme'
-      );
-      if (data) {
+    try {
+      const data = await GenerateNewIdForProposedMobilityProgramme('https://localhost:5001/spGenerateNewIdForProposedMobilityProgramme');
+      if (data !== null && data !== undefined) {
         setProposedMobilityProgrammeID(data);
+      } else {
+        throw new Error('No data received for proposed mobility programme ID');
       }
-    };
-    fetchProposedMobilityProgrammeID();
+    } catch (error) {
+      console.error('Error generating proposed mobility programme ID:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
   }
 
   async function handleGenerateNewIdForCommitment() {
-    const fetchCommitmentID = async () => {
-      const data = await GenerateNewIdForCommitment(
-        'https://localhost:5001/spGenerateNewIdForCommitment'
-      );
-      if (data) {
+    try {
+      const data = await GenerateNewIdForCommitment('https://localhost:5001/spGenerateNewIdForCommitment');
+      if (data !== null && data !== undefined) {
         setCommitmentID(data);
+      } else {
+        throw new Error('No data received for commitment ID');
+      }
+    } catch (error) {
+      console.error('Error generating commitment ID:', error);
+      // Handle error: display an error message to the user or perform other error handling tasks
+    }
+  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log('Student Info Id:', studentInfoId);
+        console.log('Proposed Mobility Programme Id:', proposedMobilityProgrammeId);
+        console.log('Commitment Id:', commitmentId);
+  
+        setStudentInfoID(Number(studentInfoId) || 0);
+        setProposedMobilityProgrammeID(Number(proposedMobilityProgrammeId) || 0);
+        setCommitmentID(Number(commitmentId) || 0);
+  
+        await Promise.all([
+          (studentInfoID === 0 || studentInfoID === undefined || studentInfoID === null) && handleGenerateNewIdForStudentInfo(),
+          (omobilityID === '' || omobilityID === undefined || omobilityID === null) && handleGenerateOmobilityId(),
+          (learningAgreementID === 0 || learningAgreementID === undefined || learningAgreementID === null) && handleGenerateNewIdForLearningAgreement(),
+          
+          handleGenerateNewIdForSendingInstitutionInfo(),
+          handleGenerateNewIdForReceivingInstitutionInfo(),
+          handleGenerateNewIdForProposedMobilityProgramme(),
+          handleGenerateNewIdForCommitment(),
+        ]);
+      } catch (error) {
+        console.error('Error generating data:', error);
+        // Set an error state variable and display an error message to the user
+      } finally {
+        // Set a loading state variable to false to indicate that the data fetching is complete
       }
     };
-    fetchCommitmentID();
-  }
-
+  
+    fetchData();
+  }, [studentInfoId, proposedMobilityProgrammeId, commitmentId]);
+  
   useEffect(() => {
-    handleGenerateOmobilityId();
-    handleGenerateNewIdForLearningAgreement();
-    handleGenerateNewIdForStudentInfo();
-    handleGenerateNewIdForSendingInstitutionInfo();
-    handleGenerateNewIdForReceivingInstitutionInfo();
-    handleGenerateNewIdForProposedMobilityProgramme();
-    handleGenerateNewIdForCommitment();
-  }, []);
+    const fetchData = async () => {
+      try {
+
+  
+        await Promise.all([
+          console.log('Student Info ID:', studentInfoID),
+          
+          handleGetStudentInfoById(),
+          
+        ]);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        // Set an error state variable and display an error message to the user
+      } finally {
+        // Set a loading state variable to false to indicate that the data fetching is complete
+      }
+    };
+  
+    fetchData();
+  }, [studentInfoID]);
 
   return (
     <Tabs variant='colorful' colorScheme='gray'>
