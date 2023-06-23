1f67af574f0c0147cb774d507164da76042eace6
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-24 01:44:43 +0300

    Create virtualCourseRequest.ts

---
+++ models/request/virtualCourseRequest.ts
@@ -0,0 +1,14 @@
+export interface VirtualCourseRequest {
+    courseTitle: string;
+    courseCreditType_id: number;
+    courseCreditValue: number;
+    numberOfTerms: number;
+    totalNumberOfTerms: number;
+    courseCode: string;
+    recognitionConditions?: string;
+    courseShortDescription?: string;
+    tableType: string;
+    isApproved: number;
+    proposedMobilityProgramme_id: number;
+    virtualComponent_id: number;
+}
