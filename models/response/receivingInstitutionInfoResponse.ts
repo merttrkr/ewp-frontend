4ebdbd3188075886516fcbd15db97dc0623702b1
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2023-06-24 20:25:15 +0300

    Create receivingInstitutionInfoResponse.ts

---
+++ models/response/receivingInstitutionInfoResponse.ts
@@ -0,0 +1,12 @@
+export interface ReceivingInstitutionInfoResponse {
+    id: number;
+    university_id: number;
+    universityDepartment_id: number;
+    academicYear_id: number;
+    academicPersonnelContactName: string;
+    academicPersonnelContactSurname: string;
+    academicPersonnelContactEmail: string;
+    phoneNumberE164: string;
+    phoneNumberExt: string;
+    heiId: string;
+}
