cca2eb30df0f3ae691721a8f25ce440bbf5cbbeb
ilaydaozel <ilaydaoozel@gmail.com>
2023-06-26 01:27:04 +0300

    Update SignatureResponse

--- models/response/signatureResponse.ts
+++ models/response/signatureResponse.ts
@@ -1,12 +1,12 @@
-interface SignatureResponse {
-    studentSignatureInBase64: string;
-    sendingInstitutionIndividualResponsibleFullname: string;
-    sendingInstitutionIndividualResponsiblePosition: string;
-    sendingInstitutionIndividualResponsibleEmail: string;
-    signatureForReceivingInstitutionIndividualResponsibleInBase64: string | null;
-    receivingInstitutionIndividualResponsibleFullname: string | null;
-    receivingInstitutionIndividualResponsiblePosition: string | null;
-    receivingInstitutionIndividualResponsibleEmail: string | null;
-    commentForRejection: string | null;
-  }
-  
+export interface SignatureResponse {
+  studentSignatureInBase64: string;
+  signatureForSendingInstitutionIndividualResponsibleInBase64: string;
+  sendingInstitutionIndividualResponsibleFullname: string;
+  sendingInstitutionIndividualResponsiblePosition: string;
+  sendingInstitutionIndividualResponsibleEmail: string;
+  signatureForReceivingInstitutionIndividualResponsibleInBase64: string | null;
+  receivingInstitutionIndividualResponsibleFullname: string | null;
+  receivingInstitutionIndividualResponsiblePosition: string | null;
+  receivingInstitutionIndividualResponsibleEmail: string | null;
+  commentForRejection: string | null;
+}
